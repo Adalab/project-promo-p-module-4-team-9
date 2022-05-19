313f2d0a64e60d75bdf3e21525e25a81e56201a3
GemaMesasV <gema.mesas.v@gmail.com>
2022-05-19 12:47:31 +0200

    create database and use it in endpoints

--- src/index.js
+++ src/index.js
@@ -1,39 +1,57 @@
 // Fichero src/index.js
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require('express');
-const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const Database = require("better-sqlite3");
 // Creamos el servidor
 const server = express();
 // Configuramos el servidor
 server.use(cors());
 server.use(
   express.json({
-    limit: '10mb',
+    limit: "10mb",
   })
 );
 
-server.set('view engine', 'ejs');
-
+server.set("view engine", "ejs");
+//indicamos database
+const db = new Database("./src/db/card.db");
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http:/localhost:${serverPort}`);
 });
-const savedCards = [];
+// const savedCards = [];
 // Escribimos los endpoints que queramos
-server.post('/card', (req, res) => {
+server.post("/card", (req, res) => {
   if (
-    req.body.name !== '' &&
-    req.body.email !== '' &&
-    req.body.job !== '' &&
-    req.body.photo !== '' &&
-    req.body.phone !== '' &&
-    req.body.linkedin !== '' &&
-    req.body.github !== ''
+    req.body.name !== "" &&
+    req.body.email !== "" &&
+    req.body.job !== "" &&
+    req.body.photo !== "" &&
+    req.body.phone !== "" &&
+    req.body.linkedin !== "" &&
+    req.body.github !== ""
   ) {
     const newCard = { ...req.body, id: uuidv4() };
-    savedCards.push(newCard);
+    // savedCards.push(newCard);
+    const query = db.prepare(
+      "INSERT INTO card(palette, name, email, job, photo, phone, linkedin, github, uuid) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"
+      
+    );
+    console.log(newCard.palette);
+    const result = query.run(
+      newCard.palette,
+      newCard.name,
+      newCard.email,
+      newCard.job,
+      newCard.photo,
+      newCard.phone,
+      newCard.linkedin,
+      newCard.github,
+      newCard.id
+    );
     const responseSuccess = {
       success: true,
       cardURL: `http://localhost:4000/card/${newCard.id}`,
@@ -42,20 +60,22 @@ server.post('/card', (req, res) => {
   } else {
     const responseError = {
       success: false,
-      error: 'Faltan parámetros',
+      error: "Faltan parámetros",
     };
     res.json(responseError);
   }
 });
 
-server.get('/card/:id', (req, res) => {
+server.get("/card/:id", (req, res) => {
   console.log(req.params.id);
-  const userCard = savedCards.find((card) => card.id === req.params.id);
-  res.render('card', userCard);
+  // const userCard = savedCards.find((card) => card.id === req.params.id);
+  const query = db.prepare('SELECT * FROM card WHERE uuid = ?')
+  const userCard = query.get(req.params.id)
+  res.render("card", userCard);
 });
 
-const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
-const staticServerPathStyles = './src/public-styles';
+const staticServerPathStyles = "./src/public-styles";
 server.use(express.static(staticServerPathStyles));
